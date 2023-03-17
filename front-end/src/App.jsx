6f769ef36727ac747f71178e4b9ee73dc9ae6eba
mohit <mravind2@asu.edu>
2023-03-17 02:08:03 -0700

    login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './landing/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
--- front-end/src/landing/PartnerSection.jsx
+++ front-end/src/landing/PartnerSection.jsx
@@ -47,33 +47,7 @@ export default function PartnerSection() {
               height={48}
             />
           </div>
-          <div className="relative isolate px-6 pt-14 lg:px-8">
-  <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
-    <svg
-      className="relative left-0 -z-10 h-[25rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+30rem)] sm:h-[50rem]"
-      viewBox="0 0 1155 678"
-    >
-      <path
-        fill="url(#grad)"
-        fillOpacity=".3"
-        d="M1155 0L-78.208 474.645 0 438.341l558.954 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-      />
-      <defs>
-        <linearGradient
-          id="grad"
-          x1="0"
-          x2="1"
-          y1=".177"
-          y2="474.645"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#3C7CB7" />
-          <stop offset={1} stopColor="#2962A3" />
-        </linearGradient>
-      </defs>
-    </svg>
-  </div>
-</div>
+          
 
 
         </div>
