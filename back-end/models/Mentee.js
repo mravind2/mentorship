fe663cfd4ee9b8ad2839dbf12bb9c0b971fb74bb
mravind2 <mravind2@asu.edu>
2023-03-20 18:44:09 -0700

    account

---
+++ back-end/models/Mentee.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const menteeSchema = new mongoose.Schema({
+    name: {type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    email: String,
+    linkedin: String,
+    photos: [String],
+    description: String,
+});
+
+const menteeModel = mongoose.model('Mentees', menteeSchema);
+
+module.exports = MenteeModel;
