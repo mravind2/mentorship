c92e25eb8ebbe956a900c1c0df3635aa809bc4e1
sgpham <sgpham@asu.edu>
2023-03-28 15:38:29 -0700

    Profile user

--- front-end/src/pages/ProfilePage.jsx
+++ front-end/src/pages/ProfilePage.jsx
@@ -53,7 +53,7 @@ export default function ProfilePage(){
                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                     </svg>
                     <span>Edit profile</span>
-
+                    
                 </Link>
             </div>
             )}
@@ -89,6 +89,56 @@ export default function ProfilePage(){
                     </form>
                 </div>
             )}
+            <div class="p-16">
+<div class="p-8 bg-white shadow mt-24">
+  <div class="grid grid-cols-1 md:grid-cols-3">
+    <div class="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
+      <div>
+        <p class="font-bold text-gray-700 text-xl">22</p>
+        <p class="text-gray-400">Friends</p>
+      </div>
+      <div>
+           <p class="font-bold text-gray-700 text-xl">10</p>
+        <p class="text-gray-400">Photos</p>
+      </div>
+          <div>
+           <p class="font-bold text-gray-700 text-xl">89</p>
+        <p class="text-gray-400">Comments</p>
+      </div>
+    </div>
+    <div class="relative">
+      <div class="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
+<svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-24" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+</svg>
+      </div>
+    </div>
+
+    <div class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+        <button class="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+            Connect
+        </button>
+        <button class="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+            Message
+        </button>
+    </div>
+  </div>
+
+  <div class="mt-20 text-center border-b pb-12">
+    <h1 class="text-4xl font-medium text-gray-700">Son Pham, <span class="font-light text-gray-500">27</span></h1>
+    <p class="font-light text-gray-600 mt-3">Tempe, Arizona</p>
+    <p class="mt-8 text-gray-500">Solution Manager - Creative Tim Officer</p>
+    <p class="mt-2 text-gray-500">Arizobna State University</p>
+  </div>
+  <div class="mt-12 flex flex-col justify-center">
+    <p class="text-gray-600 text-center font-light lg:px-16">An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
+    <button class="text-indigo-500 py-2 px-4  font-medium mt-4">
+        Show more
+    </button>
+  </div>
+
+</div>
+</div>
         </div>
     )
 }
