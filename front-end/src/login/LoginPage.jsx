9f0852c30990aaefc39d7c76bee1823de9e5190a
sgpham <sgpham@asu.edu>
2023-04-04 14:59:35 -0700

    Toggle_finish

--- front-end/src/login/LoginPage.jsx
+++ front-end/src/login/LoginPage.jsx
@@ -4,41 +4,43 @@ import HeaderSection from '../landing/HeaderSection';
 import axios from 'axios';
 import { UserContext } from '../UserContext';
 
-
-
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
-  const {setUser} = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
+
   async function handleLoginSubmit(e) {
     e.preventDefault();
     try {
-      const {data} = await axios.post('/login', { email, password });
+      const { data } = await axios.post('/login', { email, password });
       setUser(data);
       setRedirect(true);
     } catch (e) {
       alert('Login failed');
     }
   }
-  
 
-  if (redirect) {
-    return <Navigate to="/home" />; // For version 6
-    // return <Redirect to="/home" />; // For version 5
+  function handleToggle() {
+    const toggle = document.getElementById('toggle');
+    const dot = document.querySelector('.dot');
+    if (toggle.checked) {
+      dot.style.transform = 'translateX(100%)';
+    } else {
+      dot.style.transform = 'translateX(0%)';
+    }
   }
 
-
-
+  if (redirect) {
+    return <Navigate to="/home" />;
+  }
 
   return (
     <div className="flex flex-col justify-center items-center mt-4 pt-48 max-w-md mx-auto">
-      <HeaderSection/>
+      <HeaderSection />
       <h1 className="text-4xl text-center mb-4">Login</h1>
-  
 
       <form className="max-w-mf mx-auto " onSubmit={handleLoginSubmit}>
-
         <input
           type="email"
           placeholder="Email"
@@ -53,11 +55,16 @@ function LoginPage() {
           onChange={(ev) => setPassword(ev.target.value)}
           className="w-full border my-1 py-2 px-3 rounded-2xl"
         />
-        <div class="flex items-center justify-center py-2">
+        <div class="flex items-center justify-center py-4">
           <span class="text-gray-700 font-medium">Mentee</span>
           <label for="toggle" class="flex items-center ml-2 cursor-pointer">
             <div class="relative">
-              <input id="toggle" type="checkbox" class="sr-only" />
+              <input
+                id="toggle"
+                type="checkbox"
+                class="sr-only"
+                onClick={handleToggle}
+              />
               <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
               <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
             </div>
@@ -67,7 +74,7 @@ function LoginPage() {
         <button className="bg-indigo-600 hover:bg-indigo-700 p-2 w-full text-white rounded-2xl">
           Login
         </button>
-        
+
         <div className="text-center py-2 text-gray-500">
           Don't have an account yet?{' '}
           <Link className="underline text-black" to="/register">
@@ -75,7 +82,6 @@ function LoginPage() {
           </Link>
         </div>
       </form>
-      
     </div>
   );
 }
