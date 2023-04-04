7802ca00cc6e2cbfc307594a8192d921a23b95c2
sgpham <sgpham@asu.edu>
2023-04-03 21:35:34 -0700

    Update RegisterPage.jsx

--- front-end/src/login/RegisterPage.jsx
+++ front-end/src/login/RegisterPage.jsx
@@ -38,7 +38,18 @@ function RegisterPage() {
                 <button className="bg-indigo-600 hover:bg-indigo-700 p-2 w-full text-white rounded-2xl">Register</button>
                 <div className="text-center py-2 text-gray-500">
                     Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
-                </div> 
+                </div>
+                <div class="flex items-center justify-center py-4">
+                  <span class="text-gray-700 font-medium">Mentee</span>
+                  <label for="toggle" class="flex items-center ml-2 cursor-pointer">
+                    <div class="relative">
+                      <input id="toggle" type="checkbox" class="sr-only" />
+                      <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                      <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                    </div>
+                  </label>
+                  <span class="ml-2 text-gray-700 font-medium">Mentee</span>
+                </div>
             </form>
         </div>
   );
