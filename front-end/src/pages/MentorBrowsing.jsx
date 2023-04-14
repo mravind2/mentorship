24f309f17e4c610bce9705cda8b679c14720ecb7
sgpham <sgpham@asu.edu>
2023-04-13 22:41:06 -0700

    A

--- front-end/src/pages/MentorBrowsing.jsx
+++ front-end/src/pages/MentorBrowsing.jsx
@@ -19,11 +19,11 @@ export default function MentorBrowsing() {
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {mentors.map((mentor) => (
           <a key={mentor._id} href="#" className="group">
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+            <div className="w-full overflow-hidden rounded-lg bg-gray-200 flex justify-center items-center xl:aspect-h-8 xl:aspect-w-7">
               <img
                 src={mentor.imageSrc}
                 alt={mentor.imageAlt}
-                className="h-full w-full object-contain object-center group-hover:opacity-75 bg-white"
+                className="w-full h-80 object-cover object-center rounded group-hover:opacity-75 bg-white"
               />
             </div>
             <h3 className="mt-4 text-lg font-medium text-gray-700">{mentor.name}</h3>
