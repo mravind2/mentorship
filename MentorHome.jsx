9a47421a420ea4ae8594dc7290e5e826d6207f72
Hatsura0 <114840232+Hatsura0@users.noreply.github.com>
2023-04-13 17:57:31 -0700

    Add files via upload
    Now it all works, I just have two little snags. When signed in as a mentor, clicking profile, session, then profile again fsr displays the mentee header. And 2, the mentee browsing page isn't displaying a header.

---
+++ MentorHome.jsx
@@ -0,0 +1,12 @@
+import MentorHeader from "./MentorHeader";
+import FeaturesZigZag from './FeaturesZigzag';
+
+
+export default function MentorHome(){
+    return(      
+        <div>
+            <MentorHeader/>    
+            <FeaturesZigZag />  
+        </div>
+    )
+}
